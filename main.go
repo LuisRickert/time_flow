f161a1b1df8a2517596d2c709ca71defba96e9d0
luis, rickert <rickert.luis@googlemail.com>
2022-11-17 00:35:50 +0100

    fix: progress save

--- main.go
+++ main.go
@@ -4,13 +4,7 @@ import (
 	"fmt"
 	"net/http"
 	"net/http/httputil"
-	"time"
 )
-
-type time_entry struct {
-	start, stops time.Time
-	desc, p_name string
-}
 
 type toggle_data struct {
 	url, apiToken, apiPass, user_agent, workspace_id string
@@ -24,6 +18,11 @@ func main() {
 		user_agent:   "rickert.luis@googlemail.com",
 		workspace_id: "3921935",
 	}
+	etry := Entry{
+		desc:         "demo",
+		project_name: "demo",
+		wbs:          "demo",
+	}
 
 	client := &http.Client{}
 	req, _ := http.NewRequest(http.MethodGet, data.url+"?"+"user_agent="+data.user_agent+"&"+"workspace_id="+data.workspace_id, nil)
@@ -35,12 +34,16 @@ func main() {
 	//req.URL.RawQuery = q.Encode()
 
 	fmt.Println(req.URL.RawQuery)
-	res, _ := client.Do(req)
+	res, err := client.Do(req)
 
-	defer res.Body.Close()
-	fmt.Println(httputil.DumpResponse(res, true))
+	if err != nil {
+		defer res.Body.Close()
+		fmt.Println(httputil.DumpResponse(res, true))
 
-	res.Body.Close()
+		res.Body.Close()
+	} else {
+		fmt.Println(etry)
+	}
 
 }
 
